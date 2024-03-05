f914a00cd522a1ea47fb2c1aceee73eb698efb64
ANIL <134312212+XPECTO-7@users.noreply.github.com>
2024-03-06 00:37:24 +0530

    updated 3rd color

--- lib/Colors/appcolor.dart
+++ lib/Colors/appcolor.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color appPrimary =  Color(0xFFFFD700);
   static const Color appSecondary = Color(0xff8C92AC);
-  static const Color whiteSmoke = Color(0xffF5F5F5);
+  static const Color appTertiary = Color(0xffF5F5F5);
 }
